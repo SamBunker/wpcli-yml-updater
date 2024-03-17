5994d4f4ab212575c588b3a5b27dae33bd1919d9
Not Sam <samuelbunker@live.com>
2024-03-17 10:36:47 -0400

    Updated Ko-fi link

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ LATEST_YEAR_THEME = ""
 #https://wordpress.org/themes/twentytwentyfour/
 
 SCRIPT_VERSION = "2.0"
-PERSONAL_AD = "Do you appreciate my work? Consider supporting me at {KO-FI LINK}."
+PERSONAL_AD = "Do you appreciate my work? Consider supporting me at https://ko-fi.com/notsam"
 SCRIPT_DESCRIPTION = "Script to fetch the latest WordPress Core, Plugins, and Themes from the WordPress.org and JPL websites."
